ea9f28bc02a175637bbc2a59b787d08321bade97
chikechris <cchike2017@gmail.com>
2019-09-10 16:53:52 -0400

    crud complete

--- routes/carRoute.js
+++ routes/carRoute.js
@@ -25,4 +25,41 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  db('cars')
+    .insert(req.body, 'id')
+    .then(([results]) => {
+      res
+        .status(200)
+        .json({ message: `record of id ${results} has been added` })
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  db('cars')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json({ message: `deleted ${count} records` })
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+router.put('/:id', (req, res) => {
+  db('cars')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(results => {
+      res.status(200).json({ message: `${results} records modified` })
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
 module.exports = router
